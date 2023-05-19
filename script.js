b70144c5e2eca18a67c342d259a2c950db777f5f
Amit Kumar <106420527+amitjait@users.noreply.github.com>
2023-05-19 13:28:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+		function submit(){
+			console.log("click")
+			let name = document.getElementById("name");
+			let year = document.getElementById('year');
+			if(name.value.length > 0 && year.value.length > 0){
+				document.getElementById("url").innerText = `https://localhost:8080/?name=${name.value}&year=${year.value}`
+			}else if(name.value.length > 0){
+				document.getElementById("url").innerText = `https://localhost:8080/?name=${name.value}`	
+			}else if(year.value.length > 0){
+				document.getElementById("url").innerText = `https://localhost:8080/?year=${year.value}`	
+			}else{
+				document.getElementById("url").innerText = `https://localhost:8080/`
+			}
+			
+		}
